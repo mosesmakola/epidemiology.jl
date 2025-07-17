603b1ff0013bb3bbedfc7321720ac43ab6aa6a6d
mosesmakola <mmakola76@gmail.com>
2025-07-17 14:20:39 +0100

    completed lognormal simulation with helper function, need to do documentation for lognormal and gamma simulators

--- src/utils.jl
+++ src/utils.jl
@@ -1,15 +1,23 @@
 using Distributions
 
 function meanvar_to_gamma(μ::Float64, σ²::Float64)
-    shape = shape = (μ^2) / σ²
+    shape = (μ^2) / σ²
     scale = σ² / μ
     return (shape, scale)
 end
-
 
 function simulate_gamma(n::Int, μ::Float64, σ²::Float64)
     shape, scale = meanvar_to_gamma(μ, σ²)
     return rand(Gamma(shape, scale), n)
 end
 
-function simulate_lognormal(n::Int, μ::Float64, σ::Float64)
+function meanvar_to_lognormal(μ::Float64, σ²::Float64)
+    sdlog = sqrt(log(1 + σ² / μ^2))
+    meanlog = log(μ) - 0.5 * sdlog^2
+    return (meanlog, sdlog)
+end
+
+function simulate_lognormal(n::Int, μ::Float64, σ²::Float64)
+    meanlog, sdlog = meanvar_to_lognormal(μ, σ²)
+    return rand(LogNormal(meanlog, sdlog), n)
+end
