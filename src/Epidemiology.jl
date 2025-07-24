850babe90d037cdec3dc4bbbbfc86849f8fb65a7
mosesmakola <mmakola76@gmail.com>
2025-07-24 13:46:15 +0100

    finished docstrings and comments for probsbility distributions, moving onto delay distributions

--- src/Epidemiology.jl
+++ src/Epidemiology.jl
@@ -3,8 +3,22 @@ module Epidemiology
 include("utils.jl")
 include("models.jl")
 
-greet() = print("Hello World!")
+"""
+    build_model(dist::Symbol, y::Vector{<Real})
 
-export greet
+Select model to build based on symbol provided.
+"""
+# Select model based on symbolwhy
+function build_model(dist::Symbol, y::Vector{<:Real})
+    if dist == :gamma
+        return gamma_fit(y)
+    elseif dist == :lognormal
+        return lognormal_fit(y)
+    else
+        error("Unsupported Distribution")
+    end
+end
+
+export build_model
 
 end # module epidemiology
--- src/utils.jl
+++ src/utils.jl
@@ -1,27 +1,53 @@
 using Distributions
 
-# Convert mean & variance to Gamma(Shape, scale)
-function meanvar_to_gamma(μ::Float64, σ²::Float64)
+"""
+    meanvar_to_gamma(μ::Real, σ²::Real) -> (shape, scale)
+
+Convert mean "μ" and varaince "σ²" to Gamma(shape, scale) to the parameters of a Gamma distribution.
+
+"""
+function meanvar_to_gamma(μ::Real, σ²::Real)
+    # shape = mean^2 / σ²
     shape = (μ^2) / σ²
+    # scale = variance / mean
     scale = σ² / μ
     return (shape, scale)
 end
 
-# Generate n samples from Gamma(μ, σ²)
-function simulate_gamma(n::Int, μ::Float64, σ²::Float64)
+"""
+    simulate_gamma(n::Int, μ::Real, σ²::Real) -> Vector{<Real}
+
+Generate n random samples from from a Gamma distribution with given mean "μ" and variance "σ²".
+
+"""
+function simulate_gamma(n::Int, μ::Real, σ²::Real)
+    # Convert given mean and variance to shape and scale for Gamma distribution
     shape, scale = meanvar_to_gamma(μ, σ²)
     return rand(Gamma(shape, scale), n)
 end
 
-# Convert mean & variance to LogNormal(meanlog, sdlog)
-function meanvar_to_lognormal(μ::Float64, σ²::Float64)
+"""
+    meanvar_to_lognormal(μ::Real, σ²::Real) -> (meanlog, sdlog)
+
+Convert mean "μ" and varaince "σ²" to LogNormal(meanlog, sdlog) to the parameters of a LogNormal distribution.
+
+"""
+function meanvar_to_lognormal(μ::Real, σ²::Real)
+    # sdlog = sqrt(log(1 + variance / mean^2))
     sdlog = sqrt(log(1 + σ² / μ^2))
+    # meanlog = log(mean) - 0.5 * sdlog^2
     meanlog = log(μ) - 0.5 * sdlog^2
     return (meanlog, sdlog)
 end
 
-# Generate n samples from LogNormal(μ, σ²)
-function simulate_lognormal(n::Int, μ::Float64, σ²::Float64)
+"""
+    simulate_lognormal(n::Int, μ::Real, σ²::Real) -> Vector{<Real}
+
+Generate n random samples from from a LogNormal distribution with given mean "μ" and variance "σ²".
+
+"""
+function simulate_lognormal(n::Int, μ::Real, σ²::Real)
+    # Convert given mean and variance to meanlog and sdlog for LogNormal distribution
     meanlog, sdlog = meanvar_to_lognormal(μ, σ²)
     return rand(LogNormal(meanlog, sdlog), n)
 end
